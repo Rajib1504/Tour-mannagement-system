6f541d1797871244bb86b1c0d6ba7cec0ff9ea50
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-09-08 00:30:48 +0530

    access token creation and use for login send responce in token

--- Backend/src/app/modules/auth/auth.service.ts
+++ Backend/src/app/modules/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Iuser } from "../user/user.interface"
 import { User } from "../user/user.model"
 import httpStatus from 'http-status-codes';
 import bcrypytjs from "bcryptjs";
+import jwt  from "jsonwebtoken";
 
 const credentialLogin = async (payload: Partial<Iuser>) => {
       const { email, password } = payload;
@@ -11,19 +12,32 @@ const credentialLogin = async (payload: Partial<Iuser>) => {
       const isUserExist = await User.findOne({ email })
       const isPasswordMatch = await bcrypytjs.compare(password as string, isUserExist?.password as string)
       if (!isUserExist) {
-            throw new AppError(httpStatus.BAD_REQUEST, "user is not exist,Register", "")
+            throw new AppError(httpStatus.BAD_REQUEST, "user is not exist,Register", "") 
       }
       if (!isPasswordMatch) {
             throw new AppError(httpStatus.BAD_REQUEST, "incorrect password", "")
       }
+const jwtPayload = {
+      userId: isUserExist._id,
+      email:isUserExist.email,  
+      role: isUserExist.role
+
+}
+
+      const accessToken = jwt.sign(jwtPayload,'secret',{
+            expiresIn:'1d'
+      })
 
       // const {password,...rest}=isUserExist;
       // return {...rest};
       return {
-            emal: isUserExist.email
+            // emal: isUserExist.email
+            accessToken
       }
 
 }
+
+//user-login-token(email,role,_id)-booking /payment /booking /payment-cancel /tour booking 
 export const AuthServices = {
       credentialLogin,
 }
